e1472a70db792636e289838f74db17ac487c26df
BautistaVidal2002 <bvidal@udesa.edu.ar>
2025-09-22 11:18:32 -0300

    Favoritos

--- src/components/Formulario/Resultados.js
+++ src/components/Formulario/Resultados.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-/* import { Link } from 'react-router-dom/cjs/react-router-dom.min'; */
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import Fav from '../../screens/Favoritos/fav';
 
 class Resultados extends Component{
     constructor(props){
@@ -7,61 +8,89 @@ class Resultados extends Component{
 
         this.state = {
             busqueda: this.props.match.params.query.toLowerCase(), 
-            resultadosFiltrados: [], 
-            resultados: []
+            resultadosPeliculas: [], 
+            resultadosSeries: [],
+            cargando: true
         }
     }
     
     componentDidMount() {
         const api = "296583e7e37a5c7294c3a04233952058"
-        fetch(`https://api.themoviedb.org/3/search/multi?api_key=${api}&query=${this.state.busqueda}`)
+        const query = this.state.busqueda
+        
+        fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${api}`)
         .then(res => res.json())
         .then(data => {
+            const peliculasFiltradas = data.results.filter(p =>
+                p.title.toLowerCase().includes(query)
+            );
             this.setState({
-                resultados: data.results
+                resultadosPeliculas: peliculasFiltradas,
+                cargando: false
             })
         })
         .catch(error => {
             console.log(error);
         })
-        this.resultadosFiltrados();
+
+        fetch(`https://api.themoviedb.org/3/trending/tv/week?api_key=${api}`)
+        .then(res => res.json())
+        .then(data => {
+            const seriesFiltradas = data.results.filter(s =>
+                s.name.toLowerCase().includes(query)
+            );
+            this.setState({
+                resultadosSeries: seriesFiltradas,
+                cargando: false
+            })
+        })
+        .catch(error => {
+            console.log(error);
+        })
     }
 
-     resultadosFiltrados(){
-        let busqueda = this.props.match.params.query.toLowerCase();
-        let resultadosFiltrados = this.state.resultados.filter(resultado => 
-            resultado.title.toLowerCase().includes(busqueda)
-        );
-
-        this.setState({
-            busqueda: busqueda,
-            resultadosFiltrados: resultadosFiltrados
-        });
-    }
-
+    
     render(){
         return(
             <>
                 <h1>Resultados de busqueda para "{this.props.match.params.query}"</h1>
+                {this.state.cargando && <p>Cargando...</p>}
+
+                <h2>Películas:</h2>
                 <section className='row cards'>
-                    {this.state.cargando && <p>Cargando...</p>}
-                    {
-                            this.state.resultados.map((elm, i) => 
-                                <article key={i} className='single-card-movie'>
-                                    <img src={`https://image.tmdb.org/t/p/w342/${elm.poster_path}`} alt="" className="card-img-top"/>
-                                    <div className='cardBody'>
-                                        <h5 className="card-title">{elm.title || elm.name}</h5>
-                                        {/* <button onClick={()=>{this.cambio(elm.id)}}>
-                                            {this.state.verDescripcion === elm.id ? "Ocultar descripción" : "Ver descripción"}
-                                        </button>
-                                        {this.state.verDescripcion === elm.id && <p className="card-text">{elm.overview}</p>}
-                                        <br></br>
-                                        <Link to={`/serie/${elm.id}`} className="btn btn-primary">Ir a detalle</Link> */}
-                                    </div>
-                                </article>
-                            )
-                        }
+                    {this.state.resultadosPeliculas.length === 0 ? (<p>No se encontraron peliculas</p>) : (
+                        this.state.resultadosPeliculas.map((p) => (
+                            <article key={p.id} className='single-card-movie'>
+                                <img src={`https://image.tmdb.org/t/p/w342/${p.poster_path}`} alt="" className="card-img-top"/>
+                                <div className='cardBody'>
+                                    <h5 className="card-title">{p.title}</h5>
+                                    <Link to={`/pelicula/${p.id}`} className="btn btn-primary">Ir a detalle</Link>
+                                    <br></br>
+                                    <Fav isSerie={false} id={p.id} />
+                                </div>
+                            </article>
+                        ))
+                    )}
                 </section>
+                    
+                <h2>Series:</h2>
+                <section className='row cards'>
+                    {this.state.resultadosSeries.length === 0 ? (<p>No se encontraron peliculas</p>) : 
+                    (
+                        this.state.resultadosSeries.map((s) => (
+                            <article key={s.id} className='single-card-tv'>
+                                <img src={`https://image.tmdb.org/t/p/w342/${s.poster_path}`} alt="" className="card-img-top"/>
+                                <div className='cardBody'>
+                                    <h5 className="card-title">{s.name}</h5>
+                                </div>
+                                <Link to={`/serie/${s.id}`} className="btn btn-primary">Ir a detalle</Link>
+                                <br></br>
+                                <Fav isSerie={true} id={s.id}/>
+                            </article>
+                        ))
+                    )}
+                </section>
+                
             </>
         )
         
